fb4038a9e7298da5799756d172b227e80ea53584
emma-ni12 <emma.ni2212@gmail.com>
2023-05-06 13:26:14 -0400

    fixed output of LF- should be correct to apply to all scripts now

--- LF.py
+++ LF.py
@@ -9,23 +9,26 @@ from sklearn.model_selection import train_test_split
 # getting the data
 data = pd.read_csv('LF_train.csv')
 # data = data.set_index("id")
+
 # getting the target labels (whether the leg is normal (0) or lame (1))
 target = data.loc[:, "LF"]
-# remove labels from the dataset
-# cat_features = data.loc[:, ["dob", "forceplate_date",
-#                             "gait", "speed", "Gait", "Speed"]]
-# encoded_cat_features = pd.get_dummies(cat_features)
-# # get the features
-# features = pd.concat([data.drop(["id", "LF", "dob", "forceplate_date", "gait", "speed", "Gait", "Speed"], axis=1),
-#                      encoded_cat_features], axis=1)
+data = data.drop(["id", "LF"], axis=1)
+print(data.loc[0])
 
-features = data.drop(["id", "LF", "dob", "forceplate_date",
-                     "gait", "speed", "Gait", "Speed"], axis=1)
+cat_cols = ["dob","forceplate_date", "gait", "speed", "Gait", "Speed"]
+data[cat_cols] = data[cat_cols].astype('category')
+
+# features = data["dob", "forceplate_date", "gait", "speed", "Gait", "Speed"].astype("category")
+
+# print("FEATURES")
+# print(features.loc[0])
 
 # split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
-    features, target, test_size=.2)
+    data, target, test_size=.2)
 
+
+print(X_train.shape)
 # print(X_train.shape)
 # create model instance
 bst = XGBClassifier(n_estimators=5000, max_depth=2,
@@ -37,11 +40,20 @@ preds = bst.predict(X_test)
 accuracy = accuracy_score(y_test, preds)
 print("Accuracy:", accuracy)
 
+
+
+
+
+
 # load the test data
 test_data = pd.read_csv('LF_test.csv')
-test_features = test_data.drop(
-    ["id", "dob", "forceplate_date", "gait", "speed", "Gait", "Speed"], axis=1)
+ids = (test_data.loc[:, "id"]).to_numpy(np.int32)
+print(ids)
+test_data = test_data.drop(["id"], axis=1)
 
+cat_cols = ["dob","forceplate_date", "gait", "speed", "Gait", "Speed"]
+test_data[cat_cols] = test_data[cat_cols].astype('category')
+print(test_data.shape)
 # remove labels from the dataset
 # test_cf = data.loc[:, ["dob", "forceplate_date",
 #                        "gait", "speed", "Gait", "Speed"]]
@@ -50,8 +62,9 @@ test_features = test_data.drop(
 # test_features = pd.concat([data.drop(["id", "dob", "forceplate_date", "gait", "speed", "Gait", "Speed"], axis=1),
 #                            encoded_test_cf], axis=1)
 # print(test_features.shape)
-test_preds = bst.predict(test_features)
-final = np.array([id, test_preds])
+test_preds = (bst.predict(test_data)).astype(int)
+final = np.transpose(np.vstack((ids, test_preds)))     
+print(final)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 
 # creating csv file
 # np.savetxt("LF_test_labels.csv", test_preds, header="id,label", delimiter=",")
